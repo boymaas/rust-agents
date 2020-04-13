faafcf5754af6eefeb876af9651871da8fa75a38
Boy Maas <boy.maas@gmail.com>
2020-04-13 08:51:23 +0200

    Commands and Health

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,50 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 
-
 use tokio::sync::mpsc::{channel,Sender,Receiver};
+use tokio::sync::oneshot;
 use tokio::time::{Delay,delay_for};
 use tokio::prelude::*;
+
+use failure::{Fallible};
 
 mod time;
 use time::{sleep};
 
 mod message_generator;
-use message_generator::{message_generator, Message};
+use message_generator::{message_generator, Message, Ctrl};
 
 mod file_sink;
 use file_sink::{file_sink};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Fallible<()> {
     let (tx,rx) = channel::<Message>(10);
 
+    let (mut ctx,crx) = channel::<Ctrl>(10);
+
     // message_generation -> file_sink
-    tokio::spawn(message_generator(tx));
+    tokio::spawn(message_generator(crx, tx));
     tokio::spawn(file_sink(rx));
 
-    sleep(2000).await;
+    sleep(2000).await; // print 20 messages
+
+
+    println!("Health message send ..");
+
+    let (rtx,rrx) = oneshot::channel();
+    ctx.send(Ctrl::Health(rtx)).await?;
+    let response = rrx.await?;
+    println!("Received health resp {:?}!", response);
+
+    sleep(2000).await; // print 20 messages
+
+    println!("Quit message send ..");
+    ctx.send(Ctrl::Quit).await?;
+
+    sleep(2000).await; // print 20 messages
+
+    println!("Exiting program");
+
+    Ok(())
 }
--- src/message_generator.rs
+++ src/message_generator.rs
@@ -1,18 +1,49 @@
 use tokio::sync::mpsc::{channel,Sender,Receiver};
+use tokio::sync::oneshot;
+use tokio::select;
 
 use crate::time::{sleep};
 
 mod message;
 pub use message::{Message};
 
-pub async fn message_generator(mut channel: Sender<Message>) {
+
+// Ctrl channel
+// Quit
+// Health -> Healty or not
+#[derive(Debug)]
+pub enum Ctrl {
+    Quit,
+    Health(oneshot::Sender<HealthResponse>)
+}
+
+#[derive(Debug)]
+pub enum HealthResponse {
+    Healthy,
+    UnHealthy
+}
+
+pub async fn message_generator(mut ctrl: Receiver<Ctrl>, mut channel: Sender<Message>) {
     loop {
-        match channel.send(Message::Hello).await {
-            Ok(()) => sleep(100).await,
-            Err(_) => {
-                eprintln!("Error sending message");
-                break;
+        tokio::select! {
+            msg = channel.send(Message::Hello) =>
+                match msg  {
+                    Ok(()) => sleep(100).await,
+                    Err(_) => {
+                        eprintln!("Error sending message");
+                        break;
+                    }
+                },
+            ctl = ctrl.recv() => {
+                match ctl {
+                    Some(Ctrl::Quit) => break,
+                    Some(Ctrl::Health(rtx)) => {
+                        rtx.send(HealthResponse::Healthy).unwrap()
+                    }
+                    None => break // all senders have dropped
+                } 
             }
         }
     }
+    println!("Message generator stopped");
 }
