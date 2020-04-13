31493b844d6a6bb523183faef6b3a33f0a11f77c
Boy Maas <boy.maas@gmail.com>
2020-04-13 21:23:15 +0200

    Episode 5

--- src/file_sink.rs
+++ src/file_sink.rs
@@ -1,7 +1,23 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::fmt::Debug;
+
+use failure::{Fallible};
+use serde::{Serialize, Deserialize};
+
 use tokio::sync::mpsc::{channel,Sender,Receiver};
 
-pub async fn file_sink<T: core::fmt::Debug>(mut channel: Receiver<T>) {
+pub async fn file_sink<T: Debug + Serialize>(mut channel: Receiver<T>) -> Fallible<()> {
+    // open a file
+    let mut file = File::create("data.bin")?;
+
     while let Some(msg) = channel.recv().await {
         println!("Writing to file {:?}", msg);
+        // write the variable in binary format
+        // to a file
+        file.write(&bincode::serialize(&msg)?)?;
     }
+
+    // close the file on exit
+    Ok(())
 }
